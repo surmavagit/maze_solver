764fcbfd98ce67595a177964bef644e49fd80082
surmavagit <georgesurmava@gmail.com>
2023-08-26 22:46:39 +0200

    add point, line classes

--- maze_solver.py
+++ maze_solver.py
@@ -24,8 +24,37 @@ class Window:
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.start.x,
+            self.start.y,
+            self.end.x,
+            self.end.y,
+            fill=fill_color,
+            width=2
+            )
+        canvas.pack()
+
+
 def main():
     win = Window(800, 600)
+    point_A = Point(10, 30)
+    point_B = Point(200, 200)
+    line_one = Line(point_A, point_B)
+    win.draw_line(line_one, "red")
     win.wait_for_close()
 
 main()
